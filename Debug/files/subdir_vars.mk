83ebe61ac465bb9b83fead76f9bdcf66bd2d8a46
nileshbu7 <nileshupadhye392@gmail.com>
2017-05-06 20:22:33 +0530

    Add files via upload

---
+++ Debug/files/subdir_vars.mk
@@ -0,0 +1,36 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../files/Delay.c \
+../files/SWITCH.c \
+../files/UART.c 
+
+C_DEPS += \
+./files/Delay.d \
+./files/SWITCH.d \
+./files/UART.d 
+
+OBJS += \
+./files/Delay.o \
+./files/SWITCH.o \
+./files/UART.o 
+
+OBJS__QUOTED += \
+"files/Delay.o" \
+"files/SWITCH.o" \
+"files/UART.o" 
+
+C_DEPS__QUOTED += \
+"files/Delay.d" \
+"files/SWITCH.d" \
+"files/UART.d" 
+
+C_SRCS__QUOTED += \
+"../files/Delay.c" \
+"../files/SWITCH.c" \
+"../files/UART.c" 
+
+
