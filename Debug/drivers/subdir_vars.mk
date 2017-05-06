3b7c55da88373ebf12294ef07c7b5e5139d50eeb
nileshbu7 <nileshupadhye392@gmail.com>
2017-05-06 20:21:05 +0530

    Add files via upload

---
+++ Debug/drivers/subdir_vars.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+/home/akshay/ti/TivaWare_C_Series-2.1.3.156/examples/boards/ek-tm4c123gxl-boostxl-kentec-s1/drivers/Kentec320x240x16_ssd2119_spi.c \
+/home/akshay/ti/TivaWare_C_Series-2.1.3.156/examples/boards/ek-tm4c123gxl-boostxl-kentec-s1/drivers/touch.c 
+
+C_DEPS += \
+./drivers/Kentec320x240x16_ssd2119_spi.d \
+./drivers/touch.d 
+
+OBJS += \
+./drivers/Kentec320x240x16_ssd2119_spi.o \
+./drivers/touch.o 
+
+OBJS__QUOTED += \
+"drivers/Kentec320x240x16_ssd2119_spi.o" \
+"drivers/touch.o" 
+
+C_DEPS__QUOTED += \
+"drivers/Kentec320x240x16_ssd2119_spi.d" \
+"drivers/touch.d" 
+
+C_SRCS__QUOTED += \
+"/home/akshay/ti/TivaWare_C_Series-2.1.3.156/examples/boards/ek-tm4c123gxl-boostxl-kentec-s1/drivers/Kentec320x240x16_ssd2119_spi.c" \
+"/home/akshay/ti/TivaWare_C_Series-2.1.3.156/examples/boards/ek-tm4c123gxl-boostxl-kentec-s1/drivers/touch.c" 
+
+
