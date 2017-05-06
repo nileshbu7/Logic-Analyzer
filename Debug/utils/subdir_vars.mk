052a82e9cc2f1a0bbf597781bd10324819adb298
nileshbu7 <nileshupadhye392@gmail.com>
2017-05-06 20:23:56 +0530

    Add files via upload

---
+++ Debug/utils/subdir_vars.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../utils/ustdlib.c 
+
+C_DEPS += \
+./utils/ustdlib.d 
+
+OBJS += \
+./utils/ustdlib.o 
+
+OBJS__QUOTED += \
+"utils/ustdlib.o" 
+
+C_DEPS__QUOTED += \
+"utils/ustdlib.d" 
+
+C_SRCS__QUOTED += \
+"../utils/ustdlib.c" 
+
+
